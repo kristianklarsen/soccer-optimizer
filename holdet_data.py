21708b8bd0a9ee71a30c933fc1441675051efcc5
KristianKjeldgaard <kristian12a@hotmail.com>
2023-06-12 22:26:07 +0200

    - corrections to HoldetData

--- holdet_data.py
+++ holdet_data.py
@@ -3,46 +3,37 @@ import json
 import pandas as pd
 
 
-class HoldetData():
+class HoldetData:
     """Data import class from Holdet.dk."""
 
     # For now defaults to select game "Super Manager" for edition "Spring 2023".
     def __init__(self, game_id: int = 650):
         self.game_id = game_id
-        self.game_data = self.get_game_data(self.game_id)
-        self.tournament_data = self.get_tournament_data(self.game_data['tournament']['id'])
-        self.ruleset_data = self.get_ruleset_data(self.game_data['ruleset']['id'])
-        self.player_data = self.get_player_data(self.tournament_data, self.ruleset_data)
+        self.game_data = self.get_game_data()
+        self.tournament_data = self.get_tournament_data()
+        self.ruleset_data = self.get_ruleset_data()
+        self.player_data = self.get_player_data()
 
-    @staticmethod
-    def get_game_data(game_id) -> dict:
+    def get_game_data(self) -> dict:
         game_data = requests.get(
-            f"https://api.holdet.dk/catalog/games/{game_id}?v=3&appid=holdet&culture=da-DK")
+            f"https://api.holdet.dk/catalog/games/{self.game_id}?v=3&appid=holdet&culture=da-DK")
         game_data_dict = json.loads(game_data.text)
         return game_data_dict
 
-    @staticmethod
-    def get_tournament_data(tournament_id: int) -> dict:
+    def get_tournament_data(self) -> dict:
         tournament_data = requests.get(
-            f"https://api.holdet.dk/tournaments/{tournament_id}?appid=holdet&culture=da-DK")
+            f"https://api.holdet.dk/tournaments/{self.game_data['tournament']['id']}?appid=holdet&culture=da-DK")
         tournament_data_dict = json.loads(tournament_data.text)
         return tournament_data_dict
 
-    @staticmethod
-    def get_ruleset_data(ruleset_id: int) -> dict:
+    def get_ruleset_data(self) -> dict:
         ruleset_response = requests.get(
-            f"https://api.holdet.dk/rulesets/{ruleset_id}?appid=holdet&culture=da-DK")
+            f"https://api.holdet.dk/rulesets/{self.game_data['ruleset']['id']}?appid=holdet&culture=da-DK")
         ruleset_dict = json.loads(ruleset_response.text)
         return ruleset_dict
 
-    @staticmethod
-    def get_player_data(tournament_data: dict, ruleset_data: dict) -> pd.DataFrame:
-        """
-        Get player data.
-        Returns:
-
-        """
-
+    def get_player_data(self) -> pd.DataFrame:
+        tournament_data = self.tournament_data
         teams = {}
         for team in tournament_data['teams']:
             teams[team['id']] = {'team_id': team['id'],
@@ -62,6 +53,7 @@ class HoldetData():
                                      'person_id': player['person']['id'],
                                      'team_id': player['team']['id'],
                                      'position_id': player['position']['id'], }
+        ruleset_data = self.ruleset_data
         positions = {}
         for position in ruleset_data['positions']:
             positions[position['id']] = {'position_id': position['id'],
