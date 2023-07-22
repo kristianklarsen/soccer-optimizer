b32424954c8e3e44310d0385e8aa1641e15ade27
KristianKjeldgaard <kristian12a@hotmail.com>
2023-07-22 15:37:39 +0200

    - add basic methods to odds data class

--- data.py
+++ data.py
@@ -2,6 +2,7 @@ import requests
 import json
 import pandas as pd
 import http.client
+from typing import List
 
 # Look at player team performance stats data at https://github.com/C-Roensholt/ScrapeDanishSuperligaData
 #
@@ -74,17 +75,58 @@ class HoldetData:
 class OddsData:
     """Data import class for odds"""
 
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, league_id: int = 310):
             self.api_key = api_key
-
-    def bets(self):
-        conn = http.client.HTTPSConnection("v3.football.api-sports.io")
-        headers = {
+            self.request_headers = {
             'x-rapidapi-host': "v3.football.api-sports.io",
             'x-rapidapi-key': self.api_key
-        }
+            }
+            self.conn = http.client.HTTPSConnection("v3.football.api-sports.io")
+            self.league_id = league_id
 
-        conn.request("GET", "/odds/bets", headers=headers)
+    def get_leagues(self) -> List:
+        """Get the list of available leagues and cups."""
 
-        res = conn.getresponse()
+        self.conn.request("GET", "/leagues", headers=self.request_headers)
+
+        res = self.conn.getresponse()
         data = res.read()
+        results = json.loads(data)["response"]
+
+        return results
+
+    def get_fixtures(self):
+        """Get data for multiple fixtures at once."""
+
+        self.conn.request("GET", "/fixtures", headers=self.request_headers)
+
+        res = self.conn.getresponse()
+        data = res.read()
+        results = json.loads(data)["response"]
+
+        return results
+
+    def get_bets(self) -> List:
+        """Get all available bets for pre-match odds."""
+
+        self.conn.request("GET", "/odds/bets", headers=self.request_headers)
+
+        res = self.conn.getresponse()
+        data = res.read()
+        results = json.loads(data)["response"]
+
+        return results
+
+    def get_odds_fixtures_mapping(self):
+        """Get the list of available fixtures id for the endpoint odds."""
+
+        self.conn.request("GET", "/odds/mapping", headers=self.request_headers)
+
+        res = self.conn.getresponse()
+        data = res.read()
+        results = json.loads(data)["response"]
+
+        return results
+
+    def get_odds(self):
+
