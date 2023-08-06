bdb1d85ab91ab34e4c2ff28a66604bf49919fbb7
KristianKjeldgaard <kristian12a@hotmail.com>
2023-08-06 15:33:46 +0200

    - added basic vars and constr

--- optimization.py
+++ optimization.py
@@ -1,16 +1,33 @@
 from mip import *
+import pandas as pd
 
 
 class Optimization:
     """Optimization class."""
 
-    def __init__(self):
+    def __init__(self, player_data: pd.DataFrame):
         self.model = Model(sense=MAXIMIZE, solver_name=CBC)
+        self.player_data = player_data
 
     def build_model(self):
-        # add vars
+
+        # Add vars
+        player_select_vars = []
+        for idx, player in self.player_data.iterrows():
+            player_select_vars.append(
+                self.model.add_var(
+                    name=player.person_shortname,
+                    var_type=BINARY,
+                    obj=1
+                )
+            )
+
         # add constraints
-        # add obj
+        self.model.add_constr(
+            name="Exactly 11 players",
+            lin_expr=sum(player_select_vars)==11
+        )
 
     def run(self):
         # optimize and return results
+        self.model.optimize(max_seconds=30)
